9c63fb91e9082623f0948e63a6a95ef0f4e72220
Neale2 <arturo.neale@gmail.com>
2025-06-17 10:13:16 +1200

    Added function validate_value
    Validates value based on type. Type variable renamed to value_type since conflict with function type()

---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+Value is the value to check, value_type is "Menu", "Number", "Yes/No", or "Text", size defaults to no and is max value size.
+'''
+def validate_value(value, value_type, size=None):
+    #wrap in try/except
+    
+    if value == "":
+        return "Must have a value!"
+    #checking if response is quit or cancel
+    elif value.lower() == "q" or value.lower() == "c":
+        return value.lower()
+    #responses where only numbers are expected
+    elif value_type == "Menu" or value_type == "Number":
+        #will filter out any non-ints
+        if not isdigit(value):
+            return "Value is not a (whole) number!"
+        elif int(value) < 1:
+            return "Value is less than 1!"
+        elif value > size:
+            return f"Value is higher than {size}"
+        else:
+            return None
+    elif value_type == "Yes/No":
+        if value.lower == "y" or value.lower == "n" or value.lower = "no" or value.lower = "yes":
+            return None
+        else:
+            return "Value must be (y)es or (n)o".
+    #will be value type text which needs no filtering
+    else:
+        return None
+        
+        
